79d275d85f1731c2eeded8b218c8c399f786761d
samstoia <samstoia@gmail.com>
2019-05-10 16:29:49 -0700

    start formatting project list page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import '../styles/Project.css';
 
 function Project(props) {
 
 
   return (
-    <div className="projectCard" style={{
-      backgroundImage: `url(${props.image}`
-    }}>
+    <div className="projectCard">
+      <img src={props.image}></img>
       <h3>{props.title}</h3>
       <p><em>{props.description}</em></p>
       <p>{props.technologiesUsed}</p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,43 +1,49 @@
 import React from 'react';
 import Project from './Project';
+import goLeir from '../assets/images/goleir.PNG'
+import taproom from '../assets/images/taproom.PNG';
+import epiceats from "../assets/images/epiceats.PNG"
+import cloneapalooza from "../assets/images/cloneapalooza.PNG";
+import gamebuster from "../assets/images/gamebuster.PNG";
+import barbershop from "../assets/images/barbershop.PNG";
 
 let masterProjectList = [
   {
     title: 'ConeAPalooza',
     description: 'App built in Angular',
     technologiesUsed: 'Angular, Firebase',
-    image: '',
+    image: cloneapalooza,
   },
   {
     title: 'EpicEats',
     description: 'Food Ordering Platform',
     technologiesUsed: 'Angular, Firebase',
-    image: '',
+    image: epiceats,
   },
   {
     title: "Sam's Tap Room",
     description: 'App built in React',
     technologiesUsed: 'React',
-    image: '',
+    image: taproom,
   },
   {
     title: "Go'Leir",
     description: 'JavaScript Text-Based RPG',
     technologiesUsed: 'JavaScript',
-    image: '',
+    image: goLeir
   },
 
   {
     title: "GameBuster",
     description: 'Game Database Search',
     technologiesUsed: 'C#, MySql, ASP.NET Core',
-    image: '',
+    image: gamebuster,
   },
   {
-    title: "HairSalon",
-    description: 'Hair Salon Client Management System',
+    title: "Barber Shop",
+    description: 'Barber Shop Client Management System',
     technologiesUsed: 'C#, MySql, ASP.NET Core',
-    image: '',
+    image: barbershop,
   },
 ]
 
@@ -50,6 +56,8 @@ function Projects() {
         <Project title={project.title}
           description={project.description}
           technologiesUsed={project.technologiesUsed}
+          image={project.image}
+          key={index}
         />
       )}
     </div>
