72e4ff0e196f16dc7a48dcad41b86fd46b78c168
samstoia <samstoia@gmail.com>
2019-05-17 11:42:44 -0700

    add about me page content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React from "react";
+import '../styles/About.css'
+import newsImage from './../assets/images/news.jpg';
+import mountains from './../assets/images/mountains.jpg';
+import coding from "./../assets/images/coding.png"
 
 function About() {
-
-
   return (
-    <div>
-      About Works
+    <div className="page-wrapper">
+
+      <div className="about-grid">
+        <div className="whatIDo">
+
+          <div className="textBlock">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, similique hic? Quasi dolor ad earum repellat, alias ducimus voluptatum a nam odio, quis cum. Dicta libero quaerat aliquam. Porro, voluptate obcaecati. Veniam quia alias animi nihil accusantium veritatis in nesciunt?
+            </p>
+          </div>
+          <div className="imgBlock">
+            <h1>What I Do</h1>
+            <img src={coding} alt="" />
+          </div>
+
+
+        </div>
+        <div className="backstory">
+          <div className="imgBlockRight">
+            <h1>My Story</h1>
+            <img src={newsImage} alt="" />
+          </div>
+
+          <div className="textBlockRight">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, similique hic? Quasi dolor ad earum repellat, alias ducimus voluptatum a nam odio, quis cum. Dicta libero quaerat aliquam. Porro, voluptate obcaecati. Veniam quia alias animi nihil accusantium veritatis in nesciunt?
+            </p>
+          </div>
+        </div>
+        <div className="otherStuff">
+          <div className="imgBlock">
+            <h1>The Other Stuff</h1>
+            <img src={mountains} alt="" />
+          </div>
+
+          <div className="textBlock">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, similique hic? Quasi dolor ad earum repellat, alias ducimus voluptatum a nam odio, quis cum. Dicta libero quaerat aliquam. Porro, voluptate obcaecati. Veniam quia alias animi nihil accusantium veritatis in nesciunt?
+            </p>
+          </div>
+
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
