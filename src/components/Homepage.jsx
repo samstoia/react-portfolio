b17baa632c1406813359399c591c4af8cb20ed52
samstoia <samstoia@gmail.com>
2019-05-17 14:36:05 -0700

    get background image on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 // import Button from 'react-bootstrap/Button';
+import '../styles/Homepage.css';
 
 function Homepage() {
   return (
 
-    <div>
-      Homepage Works
-        <Link to="/about">About Link</Link>
-      <Link to="/projects">Projects Link</Link>
-      <Link to="/contact">Contact Link</Link>
+    <div className="hero-image">
 
+      <div >
+        Homepage Works
+        <Link to="/about">About Link</Link>
+        <Link to="/projects">Projects Link</Link>
+        <Link to="/contact">Contact Link</Link>
+      </div>
 
     </div>
   );
