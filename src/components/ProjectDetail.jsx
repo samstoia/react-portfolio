28d45b78e423832dbe576cf68dd113696480f696
samstoia <samstoia@gmail.com>
2019-05-17 09:53:27 -0700

    basic styling in project detail page

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import '../styles/ProjectDetail.css';
+import Button from 'react-bootstrap/Button';
 
 const ProjectDetail = (props) => {
   let properties = props.location.state;
 
   return (
     <div className="projectDetailContainer">
-      <div className="imgDiv">
-        {/* <a href={properties.url}></img></a> */}
-        <img src={properties.image} alt="project screenshot" />
+
+      <div className="imgDiv" style={{ backgroundImage: `url(${properties.image})` }}>
       </div>
+
       <div className="detailsDiv">
         <h3>{properties.title}</h3>
 
         <p><em>{properties.technologiesUsed}</em></p>
         <p>{properties.synopsis}</p>
+        <a href={properties.url}><Button>Go To Project Page</Button></a>
       </div>
-    </div>
+    </div >
 
   );
 }
