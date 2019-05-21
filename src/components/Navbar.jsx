c810409d3cc75e4d73817dfdac2791baaffc37f3
samstoia <samstoia@gmail.com>
2019-05-20 19:34:42 -0700

    clean up sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
           </div>
 
           <div className="center-menu">
-            <a href="/">Sam Stoia</a>
+            {/* <a href="/">Sam Stoia</a> */}
           </div>
 
           <div className="right-menu">
