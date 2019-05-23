10c7b60d9f6bb8cf9ac02c30da13ff1024c49931
Guest User <Guest@epicodus-79.local>
2019-05-23 10:17:49 -0700

    Updates

---
+++ precache-manifest.9a64f1ee548c642512f95b060fdd71e6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416d032ded973d23a2bc95c075278b8b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "75ddad8a2c66446fb6d1",
+    "url": "/react-portfolio/static/css/main.9915a239.chunk.css"
+  },
+  {
+    "revision": "88147a822fc4814d352b",
+    "url": "/react-portfolio/static/js/2.402db8d8.chunk.js"
+  },
+  {
+    "revision": "75ddad8a2c66446fb6d1",
+    "url": "/react-portfolio/static/js/main.159e4c8a.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "e594ec00fa8bddfe0faf726bf3076983",
+    "url": "/react-portfolio/static/media/background.e594ec00.png"
+  },
+  {
+    "revision": "d2a20104890e35b6b6aae87a0f38eabf",
+    "url": "/react-portfolio/static/media/barbershop.d2a20104.PNG"
+  },
+  {
+    "revision": "9e2a119d845ee905dff602ddd5e7cbbf",
+    "url": "/react-portfolio/static/media/cloneapalooza.9e2a119d.PNG"
+  },
+  {
+    "revision": "60871687e1bfa0f511aa3f0b17f3cd8d",
+    "url": "/react-portfolio/static/media/coding.60871687.png"
+  },
+  {
+    "revision": "10070ca8ba39bda4486d3094f4aa7bcb",
+    "url": "/react-portfolio/static/media/epiceats.10070ca8.PNG"
+  },
+  {
+    "revision": "3487c48e33f52ffd900c5593419ec608",
+    "url": "/react-portfolio/static/media/gamebuster.3487c48e.PNG"
+  },
+  {
+    "revision": "d5318306674779e885ba385096c40585",
+    "url": "/react-portfolio/static/media/goleir.d5318306.PNG"
+  },
+  {
+    "revision": "c66acb6a9f9f56f909b9ef219a9ab6f2",
+    "url": "/react-portfolio/static/media/mountains.c66acb6a.jpg"
+  },
+  {
+    "revision": "64045d71afbbf770e113cdec69e3b43e",
+    "url": "/react-portfolio/static/media/news.64045d71.jpg"
+  },
+  {
+    "revision": "357e3f5df79953ecbfc2abc4cbccc756",
+    "url": "/react-portfolio/static/media/taproom.357e3f5d.PNG"
+  }
+]);
